3944708901454cd23cdc6711d7c6ddffc9c2f562
Dizlybob <greyhip@gmail.com>
2025-12-24 17:21:22 +0500

    Homework - ООП. Полиморфизм. Интерфейсы. Доработка

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -8,31 +8,19 @@ public class SearchEngine {
     }
 
     public void search(String target) {
-        Searchable[] searchables = new Searchable[5];
+        Searchable[] similarObjects = new Searchable[5];
         byte a = 0;
         for (int i=0; i< searchablesObjects.length; i++) {
             if (searchablesObjects[i] != null && searchablesObjects[i].searchTerm().contains(target)) {
-                searchables[a] = searchablesObjects[i];
+                similarObjects[a] = searchablesObjects[i];
                 a++;
             }
-            if (a > 4) {
+            if (a >= 5) {
                 break;
             }
         }
-        if (searchables[0] != null) {
-            System.out.println(searchables[0].toString());
-        }
-        if (searchables[1] != null) {
-            System.out.println(searchables[1].toString());
-        }
-        if (searchables[2] != null) {
-            System.out.println(searchables[2].toString());
-        }
-        if (searchables[3] != null) {
-            System.out.println(searchables[3].toString());
-        }
-        if (searchables[4] != null) {
-            System.out.println(searchables[4].toString());
+        for (Searchable searchable : similarObjects) {
+            System.out.println(searchable);
         }
     }
 
